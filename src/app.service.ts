4eb6095d2439162150d93ec93cdb401e7aa3ccde
David Mytton <david@arcjet.com>
2024-10-18 15:37:38 +0100

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,18 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   message(): string {
-    return `
-      This entire application is protected by shield.
-      <br />
-      To explore other protections:
-      <br />
-      * GET /rate-limit
-      <br />
-      * POST a phone number to /sensitive-info
-      <br />
-      * POST an email to /email
-      <br />
-      * GET /bot with the curl command
-    `;
+    return `Hello world. See the README for instructions.`;
   }
 }
